b2666a988ff4522286abaa1e04420336aa7fbf4d
Slav Petkov <slav.petev@gmail.com>
2018-03-17 08:56:08 +0200

    Implemenred method for claculating a nations total power

--- Avatar/Avatar.Logic/Nations/Nation.cs
+++ Avatar/Avatar.Logic/Nations/Nation.cs
@@ -3,11 +3,15 @@ using Avatar.Logic.Common.Extensions;
 using Avatar.Logic.Monuments;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Avatar.Logic.Nations
 {
     public abstract class Nation
     {
+        private const double ZERO_POWER = default(double);
+        private const uint NO_MONUMENT_BONUS = default(uint);
+
         private readonly List<Bender> _benders =
             new List<Bender>();
         private readonly List<Monument> _monuments =
@@ -17,6 +21,29 @@ namespace Avatar.Logic.Nations
             _benders.Add(bender);
         public void AddMonument(Monument monument) =>
             _monuments.Add(monument);
+
+        public double CalculateTotalPower()
+        {
+            var basePower = this.CalculateBasePower();
+            var bonusPercentage = this.CalculateMonumentBonusPercentage();
+            var totalPower = (basePower / 100) * bonusPercentage;
+
+            return totalPower;
+        }
+
+        private double CalculateBasePower()
+        {
+            return !_benders.Any()
+                ? ZERO_POWER
+                : _benders.Sum(bender => bender.CalculateTotalPower());
+        }
+
+        private long CalculateMonumentBonusPercentage()
+        {
+            return !_monuments.Any()
+                ? NO_MONUMENT_BONUS
+                : _monuments.Sum(monument => monument.CalculateNationPowerIncrease());
+        }
 
         public void LoseWar()
         {
